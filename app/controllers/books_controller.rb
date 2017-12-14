061a97335e70c28321052cfcacfdb1605cb52774
Lena <fatmeow@gmail.com>
2017-12-14 17:38:08 -0500

    Step 2, add respond_to block to index of books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.csv
+      format.text
+      format.json
+    end
   end
 
 end
